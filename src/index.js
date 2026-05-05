767792c360c33e1ea75fa3191eaf7c4507aa7e11
potahi24 <yuha0204aa@gmail.com>
2026-05-05 23:56:13 +0700

    enable PWA

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-serviceWorkerRegistration.register();
+
+serviceWorkerRegistration.unregister();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
